01d73ab0d4f80a49ad8aff294a6b051d17a253ba
Eric Souza <ericsouza0801@gmail.com>
2020-05-29 12:34:58 -0300

    adde pool_cycle

--- lyra_orm/config/__init__.py
+++ lyra_orm/config/__init__.py
@@ -9,7 +9,7 @@ if "sqlite" in environ.get("DATABASE_URL"):
         environ.get("DATABASE_URL"), connect_args={"check_same_thread": False},
     )
 else:
-    engine = create_engine(environ.get("DATABASE_URL"))
+    engine = create_engine(environ.get("DATABASE_URL"), pool_recycle=60)
 
 
 Session = sessionmaker(bind=engine)
